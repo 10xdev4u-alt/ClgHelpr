07a896dfd6c797c6fbec0a681595842da4ce60a5
10xdev4u-alt <10xdev4u@gmail.com>
2026-01-06 22:59:27 +0530

    feat(backend): initialize Go Fiber server with entrypoint

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+    app := fiber.New()
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.JSON(fiber.Map{
+            "message": "Welcome to the Campus Pilot API!",
+        })
+    })
+
+    app.Listen(":8080")
+}
